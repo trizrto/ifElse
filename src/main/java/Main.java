25d35ca907b29e98f54df51bcc2708cb20ad7be5
Beatriz Huberto <169067859+trizrto@users.noreply.github.com>
2024-09-09 10:42:34 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+  
+    int numerador = 7;
+    int denominador = 3;
+    int resto = numerador % denominador;
+
+    System.out.println("O resto é: " + resto);
+  }
+
+  
+}
